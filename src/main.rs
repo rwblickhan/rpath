f0ff7af3a346363fc22ad81fd55abd0603fba232
Russell Blickhan <rwblickhan@gmail.com>
2019-03-25 13:26:26 -0700

    Add ray intersection

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use rand::prelude::*;
+use rayon::prelude::*;
+
+use std::f64::INFINITY;
 
 type Scalar = f64;
 type Point = nalgebra::Point3<Scalar>;
@@ -37,6 +40,32 @@ enum Geometry {
     },
 }
 
+impl Geometry {
+    fn intersect(&self, ray: &Ray) -> Scalar {
+        match self {
+            &Geometry::Plane { normal, offset } => {
+                let d0 = normal.dot(ray.dir.as_ref());
+                if d0 != 0.0 {
+                    let t = -1.0 * (normal.dot(&ray.origin.coords) + offset) / d0;
+                    if t > 1e-6 { t } else { 0.0 }
+                } else { 0.0 }
+            }
+            &Geometry::Sphere { center, radius } => {
+                let b = ((ray.origin - center) * 2.0).dot(ray.dir.as_ref());
+                let c = (ray.origin - center).dot(&(ray.origin - center)) - (radius * radius);
+                let disc_sq = b * b - 4.0 * c;
+                if disc_sq < 0.0 {
+                    return 0.0;
+                }
+                let disc = disc_sq.sqrt();
+                let sol1 = -b + disc;
+                let sol2 = -b - disc;
+                if sol2 > 1e-6 { sol2 / 2.0 } else { if sol1 > 1e-6 { sol1 / 2.0 } else { 0.0 } }
+            }
+        }
+    }
+}
+
 #[derive(Clone, Copy, Debug, PartialEq)]
 struct Object {
     geometry: Geometry,
@@ -48,6 +77,25 @@ struct Scene {
     objects: Vec<Object>,
 }
 
+impl Scene {
+    fn intersect(&self, ray: &Ray) -> (Scalar, Option<&Object>) {
+        self.objects.par_iter()
+            .fold(|| (INFINITY, None),
+                  |(t, obj), next_obj| {
+                      let next_obj_t = next_obj.geometry.intersect(ray);
+                      if next_obj_t > 1e-6 && next_obj_t < t {
+                          (next_obj_t, Some(next_obj))
+                      } else { (t, obj) }
+                  })
+            .reduce(|| (INFINITY, None),
+                    |(t, obj), (next_t, next_obj)| {
+                        if next_t > 1e-6 && next_t < t {
+                            (next_t, next_obj)
+                        } else { (t, obj) }
+                    })
+    }
+}
+
 fn trace(ray: &Ray, scene: &Scene, depth: u32) -> Option<Color> {
     let mut rng = rand::thread_rng();
 
@@ -56,6 +104,13 @@ fn trace(ray: &Ray, scene: &Scene, depth: u32) -> Option<Color> {
         let stop_prob = 0.1;
         if rng.gen::<f64>() <= stop_prob { return None; } else { 1.0 / 1.0 - stop_prob }
     } else { 1.0 };
+
+    // Find ray intersection
+    let (t, object) = scene.intersect(ray);
+    if object.is_none() { return None; }
+    let hit_point = &ray.origin + t * ray.dir.into_inner();
+    let hit_material = object.unwrap().material;
+    let hit_geometry = object.unwrap().geometry;
 
     None
 }
