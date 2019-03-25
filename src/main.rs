3fd950cbde16db651a81eef5e3b76d54a5443e4f
Russell Blickhan <rwblickhan@gmail.com>
2019-03-25 14:38:26 -0700

    Implement basic recursive tracing

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use rand::prelude::*;
 use rayon::prelude::*;
 
 use std::f64::INFINITY;
+use std::f64::consts::PI;
 
 type Scalar = f64;
 type Point = nalgebra::Point3<Scalar>;
@@ -64,6 +65,10 @@ impl Geometry {
             }
         }
     }
+
+    fn normal(&self, p0: &Point) -> DirVec {
+        nalgebra::Unit::new_unchecked(nalgebra::Vector3::<Scalar>::zeros())
+    }
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
@@ -96,6 +101,23 @@ impl Scene {
     }
 }
 
+fn orthonormal_system(v1: &DirVec) -> (DirVec, DirVec) {
+    let v2 = if v1.x.abs() > v1.y.abs() {
+        let inv_len = 1.0 / (v1.x * v1.x + v1.z * v1.z).sqrt();
+        nalgebra::Unit::new_normalize(nalgebra::Vector3::new(-1.0 * v1.z * inv_len, 0.0, v1.x * inv_len))
+    } else {
+        let inv_len = 1.0 / (v1.y * v1.y + v1.z * v1.z).sqrt();
+        nalgebra::Unit::new_normalize(nalgebra::Vector3::new(0.0, v1.z * inv_len, -v1.y * inv_len))
+    };
+    (v2, nalgebra::Unit::new_normalize(v1.as_ref().cross(v2.as_ref())))
+}
+
+fn sample_hemisphere_uniform(u1: Scalar, u2: Scalar) -> DirVec {
+    let r = (1.0 - u1 * u1).sqrt();
+    let phi = 2.0 * PI * u2;
+    nalgebra::Unit::new_normalize(nalgebra::Vector3::new(phi.cos() * r, phi.sin() * r, u1))
+}
+
 fn trace(ray: &Ray, scene: &Scene, depth: u32) -> Option<Color> {
     let mut rng = rand::thread_rng();
 
@@ -111,8 +133,59 @@ fn trace(ray: &Ray, scene: &Scene, depth: u32) -> Option<Color> {
     let hit_point = &ray.origin + t * ray.dir.into_inner();
     let hit_material = object.unwrap().material;
     let hit_geometry = object.unwrap().geometry;
+    let normal = hit_geometry.normal(&hit_point);
 
-    None
+    let emission = hit_material.emission;
+    let emission_color = Color::new(emission, emission, emission) * rr_factor;
+
+    match hit_material.mat_type {
+        MaterialType::Diffuse => {
+            let (rot_x, rot_y) = orthonormal_system(&normal);
+            let sampled_dir = sample_hemisphere_uniform(rng.gen(), rng.gen());
+            let rotated_dir = nalgebra::Unit::new_normalize(nalgebra::Vector3::new(
+                nalgebra::Vector3::new(rot_x.x, rot_y.x, normal.x).dot(&sampled_dir),
+                nalgebra::Vector3::new(rot_x.y, rot_y.y, normal.y).dot(&sampled_dir),
+                nalgebra::Vector3::new(rot_x.z, rot_y.z, normal.z).dot(&sampled_dir),
+            ));
+            let out_ray = Ray { origin: hit_point, dir: rotated_dir };
+            let cos = out_ray.dir.dot(&normal);
+            let indirect_color = trace(&out_ray, scene, depth + 1);
+            Some(emission_color + (indirect_color.unwrap_or(Color::zeros()).component_mul(&hit_material.color)) * cos * 0.1 * rr_factor)
+        }
+        MaterialType::Specular => {
+            let cos = ray.dir.dot(&normal);
+            let out_ray = Ray {
+                origin: hit_point,
+                dir: nalgebra::Unit::new_normalize(ray.dir.as_ref() - normal.as_ref() * (cos * 2.0)),
+            };
+            let indirect_color = trace(&out_ray, scene, depth + 1);
+            Some(emission_color + indirect_color.unwrap_or(Color::zeros()) * rr_factor)
+        }
+        MaterialType::Refractive => {
+            // TODO make refractive index configurable
+            let mut n: f64 = 1.5;
+            let mut mut_normal = normal;
+            let r0 = ((1.0 - n) / (1.0 + n)).powi(2);
+            if mut_normal.dot(ray.dir.as_ref()) > 0.0 {
+                mut_normal = nalgebra::Unit::new_normalize(mut_normal.into_inner() * -1.0);
+                n = 1.0 / n;
+            }
+            n = 1.0 / n;
+            let cos1 = mut_normal.dot(ray.dir.as_ref()) * -1.0;
+            let cos2 = 1.0 - n * n * (1.0 - cos1 * cos1);
+            let r_prob = (r0 * (1.0 - r0)) * (1.0 - cos1).powi(5);
+            let out_ray = Ray {
+                origin: hit_point,
+                dir: if cos2 > 0.0 && rng.gen::<f64>() > r_prob {
+                    nalgebra::Unit::new_normalize((ray.dir.as_ref() * n) + (mut_normal.as_ref() * (n * cos1 - cos2.sqrt())))
+                } else {
+                    nalgebra::Unit::new_normalize(ray.dir.as_ref() + mut_normal.as_ref() * (cos1 * 2.0))
+                },
+            };
+            let indirect_color = trace(&out_ray, scene, depth + 1);
+            Some(emission_color + indirect_color.unwrap_or(Color::zeros()) * 1.15 * rr_factor)
+        }
+    }
 }
 
 fn main() {
